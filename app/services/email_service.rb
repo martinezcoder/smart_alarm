f89ca78473555a17fe161e4b833dac533b22b951
Sebastian <sebastian.melgar91@gmail.com>
2016-01-04 15:34:35 -0300

    created background task to notify about expired endpoints

--- app/services/email_service.rb
+++ app/services/email_service.rb
@@ -1,7 +1,25 @@
 class EmailService
   attr_reader :client, :alarm, :email_data
 
-  def initialize()
+  def initialize(alarm, email_data)
     @client = SendGrid::Client.new(api_key: ENV['SENDGRID_API_KEY'])
+    @email_data = email_data
+    @alarm = alarm
+  end
+
+  def send
+    mail = SendGrid::Mail.new do |m|
+      m.to = email_data[:recipient_list]
+      m.from = email_data[:sender]
+      m.subject = email_data[:subject]
+      m.text = email_data[:text]
+    end
+
+    res = client.send(mail)
+    if res.code == 200
+      alarm.sent_alert = Time.now.to_i
+      alarm.retries += 1
+      alarm.save
+    end
   end
 end
